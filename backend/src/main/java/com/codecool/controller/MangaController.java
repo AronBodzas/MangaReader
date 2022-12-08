c22449d0078d38c2080394e5fc263905dba0fef0
AronBodzas <aron.bodzas42@gmail.com>
2022-12-08 14:08:11 +0100

    Refactor: removed dead code

--- backend/src/main/java/com/codecool/controller/MangaController.java
+++ backend/src/main/java/com/codecool/controller/MangaController.java
@@ -2,7 +2,6 @@ package com.codecool.controller;
 
 import com.codecool.model.Chapter;
 import com.codecool.model.Manga;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
