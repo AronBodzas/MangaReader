d1f7881e320aaaa6513e8a026c3aa607488c2b6e
AronBodzas <aron.bodzas42@gmail.com>
2022-12-08 13:52:06 +0100

    Feat: adding Carousel to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Router} from "react-router-dom";
 import App from "./App";
 import Header from "./Header";
+import Carousel from "./Carousel";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Header />
+        <Carousel />
         <BrowserRouter>
             <App/>
         </BrowserRouter>
