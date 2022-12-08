2e91e971320cb1ab4d69ec9938f0b17ba16dcad6
AronBodzas <aron.bodzas42@gmail.com>
2022-12-08 11:25:59 +0100

    Feat: MangaList Component, design with placeholders

---
+++ frontend/src/MangaList.js
@@ -0,0 +1,153 @@
+import './Resources/mainPage.css';
+
+
+function MangaList(){
+    return (
+        <div className="card-container">
+            <div className="card">
+                <img className="cover-page" src="https://asura.gg/wp-content/uploads/2022/08/standardofReCover03.png"/>
+                <div className="empty"/>
+                <div className="title-area">
+                    <p className="title">How to optimize images in WordPress for faster loading (complete guide)</p>
+                    <p className="author">Jane Lovering</p>
+                </div>
+                <div className="empty"/>
+                <div className="tag-area">
+                    <div className="tag">
+                        <p className="tag-name">Interesting</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">WordPress</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">Blogging</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">Business</p>
+                    </div>
+                </div>
+                <div className="empty"/>
+                <p className="description">The greatest martial family, Samion. Daven, a member of the Samion family, is a one-armed martial soldier without a right arm. Despite the ridicule and contempt he got for only having a left arm, he surpassed the direct line of the Samion family like a genius. However, he got betrayed by his family and parents in the end, thus meeting a poor end. But, he was reincarnated. “I have a right arm?” He had a new family tradition. A naturally gifted body. Experience from his past life. And the god Yulion with him as well. After his reincarnation, everything changed, and a new adventure started.</p>
+                <div className="empty"/>
+                <div className="button-area">
+                    {/*<p style="flex: 1 1 0%; font-size: 24px; font-weight: 500; line-height: 36px; text-align: right; color: rgba(13, 45, 89, 1);">37 Chapters</p>*/}
+                    {/*<div className="tag-tab"/>*/}
+                    <div className="button">
+                        <p className="read-button-text">Read now</p>
+                        <div className="button-tab"/>
+                        <p className="button-arrow"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <img className="cover-page" src="https://asura.gg/wp-content/uploads/2022/08/standardofReCover03.png"/>
+                <div className="empty"/>
+                <div className="title-area">
+                    <p className="title">How to optimize images in WordPress for faster loading (complete guide)</p>
+                    <p className="author">Jane Lovering</p>
+                </div>
+                <div className="empty"/>
+                <div className="tag-area">
+                    <div className="tag">
+                        <p className="tag-name">Interesting</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">WordPress</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">Blogging</p>
+                    </div>
+                    <div className="tag-tab"/>
+                    <div className="tag">
+                        <p className="tag-name">Business</p>
+                    </div>
+                </div>
+                <div className="empty"/>
+                <p className="description">The greatest martial family, Samion. Daven, a member of the Samion family, is a one-armed martial soldier without a right arm. Despite the ridicule and contempt he got for only having a left arm, he surpassed the direct line of the Samion family like a genius. However, he got betrayed by his family and parents in the end, thus meeting a poor end. But, he was reincarnated. “I have a right arm?” He had a new family tradition. A naturally gifted body. Experience from his past life. And the god Yulion with him as well. After his reincarnation, everything changed, and a new adventure started.</p>
+                <div className="empty"/>
+                <div className="button-area">
+                    {/*<p style="flex: 1 1 0%; font-size: 24px; font-weight: 500; line-height: 36px; text-align: right; color: rgba(13, 45, 89, 1);">37 Chapters</p>*/}
+                    {/*<div className="tag-tab"/>*/}
+                    <div className="button">
+                        <p className="read-button-text">Read now</p>
+                        <div className="button-tab"/>
+                        <p className="button-arrow"></p>
+                    </div>
+                </div>
+            </div><div className="card">
+            <img className="cover-page" src="https://asura.gg/wp-content/uploads/2022/08/standardofReCover03.png"/>
+            <div className="empty"/>
+            <div className="title-area">
+                <p className="title">How to optimize images in WordPress for faster loading (complete guide)</p>
+                <p className="author">Jane Lovering</p>
+            </div>
+            <div className="empty"/>
+            <div className="tag-area">
+                <div className="tag">
+                    <p className="tag-name">Interesting</p>
+                </div>
+                <div className="tag-tab"/>
+                <div className="tag">
+                    <p className="tag-name">WordPress</p>
+                </div>
+                <div className="tag-tab"/>
+                <div className="tag">
+                    <p className="tag-name">Blogging</p>
+                </div>
+                <div className="tag-tab"/>
+                <div className="tag">
+                    <p className="tag-name">Business</p>
+                </div>
+            </div>
+            <div className="empty"/>
+            <p className="description">The greatest martial family, Samion. Daven, a member of the Samion family, is a one-armed martial soldier without a right arm. Despite the ridicule and contempt he got for only having a left arm, he surpassed the direct line of the Samion family like a genius. However, he got betrayed by his family and parents in the end, thus meeting a poor end. But, he was reincarnated. “I have a right arm?” He had a new family tradition. A naturally gifted body. Experience from his past life. And the god Yulion with him as well. After his reincarnation, everything changed, and a new adventure started.</p>
+            <div className="empty"/>
+            <div className="button-area">
+                {/*<p style="flex: 1 1 0%; font-size: 24px; font-weight: 500; line-height: 36px; text-align: right; color: rgba(13, 45, 89, 1);">37 Chapters</p>*/}
+                {/*<div className="tag-tab"/>*/}
+                <div className="button">
+                    <p className="read-button-text">Read now</p>
+                    <div className="button-tab"/>
+                    <p className="button-arrow"></p>
+                </div>
+            </div>
+        </div>
+
+            {/*<div style="width: 60px;"/>*/}
+            <div className="card">
+                <img className="cover-page" src="https://asura.gg/wp-content/uploads/2022/07/Reaper-Moon-Cover-Animation_Compressed.gif"/>
+                <div className="empty"/>
+                <div className="title-area">
+                    <p className="title">Reaper of the Drifting Moon</p>
+                    <p className="author">Siobhán Dunbar</p>
+                </div>
+                <div className="empty"/>
+                <div className="tag-area">
+                    <div className="tag">
+                        <p className="tag-name">Creativity</p>
+                    </div>
+                    {/*        <div style="width: 6px;"/>*/}
+                </div>
+                <div className="empty"/>
+                <p className="description">The long barrow was built on land previously inhabited in the Mesolithic period. It consisted of a sub-rectangular earthen tumulus, estimated to have been 15 metres (50 feet) in length, with a chamber built from sarsen megaliths on its eastern end. Both inhumed and cremated human remains were placed within this chamber during the Neolithic period, representing at least nine or ten individuals.</p>
+                <div className="empty"/>
+                <div className="button-area">
+                    {/*<p style="flex: 1 1 0%; font-size: 24px; font-weight: 500; line-height: 36px; text-align: right; color: rgba(13, 45, 89, 1);">4 min read</p>*/}
+                    {/*        <div style="width: 20px;"/>*/}
+                    <div className="button">
+                        <p className="read-button-text">Read now</p>
+                        <div className="button-tab"/>
+                        <p className="button-arrow"></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default MangaList;
