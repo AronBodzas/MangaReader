3b212e3ca62278e221feb2541cdb1a7f21e6c2b1
AronBodzas <aron.bodzas42@gmail.com>
2022-12-08 14:04:59 +0100

    Feat: URI-s for fetching

--- backend/src/main/java/com/codecool/controller/MangaController.java
+++ backend/src/main/java/com/codecool/controller/MangaController.java
@@ -1,9 +1,39 @@
 package com.codecool.controller;
 
+import com.codecool.model.Chapter;
+import com.codecool.model.Manga;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/")
 public class MangaController {
 
+
+    @GetMapping(value = "all-manga")
+    public List<Manga> getAllManga(){
+        return null;
+    }
+
+    @GetMapping(value = "{currentManga}")
+    public Manga getCurrentManga(@PathVariable String currentManga){
+        return null;
+    }
+
+    @GetMapping(value = "{currentManga}/{chapterNumber}")
+    public Chapter getMangaChapter(@PathVariable String chapterNumber, @PathVariable String currentManga){
+        return null;
+    }
+
+
+    @GetMapping(value = "{currentManga}/{chapterNumber}/{currentPage}")
+    public String getCurrentPage(@PathVariable String chapterNumber, @PathVariable String currentManga, @PathVariable String currentPage){
+        return null;
+    }
 
 }
